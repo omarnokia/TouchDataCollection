989797cdea7ba792f813ac6af40705ae69046644
Zainab M Zayed <zainab_zayed@yahoo.com>
2018-07-27 21:05:21 +0200

    edit layouts to hide status bar

--- app/src/main/java/net/dev/zainabzayed/touchdatacollection/SplashActivity.java
+++ app/src/main/java/net/dev/zainabzayed/touchdatacollection/SplashActivity.java
@@ -1,13 +1,34 @@
 package net.dev.zainabzayed.touchdatacollection;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // hide status bar
+        // If the Android version is lower than Jellybean, use this call to hide
+        // the status bar.
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+        else{
+            View decorView = getWindow().getDecorView();
+            // Hide the status bar.
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+            // Remember that you should never show the action bar if the
+            // status bar is hidden, so hide that too if necessary.
+            //ActionBar actionBar = getActionBar();
+            //actionBar.hide();
+        }
         setContentView(R.layout.activity_splash);
         Thread timerThread = new Thread() {
             @Override
